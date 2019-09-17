cbc775e9b6b399a86c7b4be6e005b0015e3bb8c3
志文 唐 <1053682192@qq.com>
2019-09-17 15:36:24 +0800

    111

---
+++ YD/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace YD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.导入文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.书签ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始ToolStripMenuItem,
+            this.书签ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(771, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导入文件ToolStripMenuItem});
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.开始ToolStripMenuItem.Text = "开始";
+            // 
+            // 导入文件ToolStripMenuItem
+            // 
+            this.导入文件ToolStripMenuItem.Name = "导入文件ToolStripMenuItem";
+            this.导入文件ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.导入文件ToolStripMenuItem.Text = "导入文件";
+            this.导入文件ToolStripMenuItem.Click += new System.EventHandler(this.导入文件ToolStripMenuItem_Click);
+            // 
+            // 书签ToolStripMenuItem
+            // 
+            this.书签ToolStripMenuItem.Name = "书签ToolStripMenuItem";
+            this.书签ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.书签ToolStripMenuItem.Text = "书签";
+            this.书签ToolStripMenuItem.Click += new System.EventHandler(this.书签ToolStripMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("华文中宋", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox.Location = new System.Drawing.Point(176, 25);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(595, 589);
+            this.textBox.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.Font = new System.Drawing.Font("华文中宋", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(0, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 589);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 614);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 导入文件ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem 书签ToolStripMenuItem;
+    }
+}
+
---
+++ YD/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<string, string> keyTxtDic = new Dictionary<string, string>();
+        private void 导入文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                keyTxtDic.Clear();
+                OpenFileDialog file = new OpenFileDialog();
+                file.Filter = "文本文件(*.txt)|*.txt";
+                file.ShowDialog();
+                string url = file.FileName;
+                string baseTxt = "";
+                if (url != "")
+                {
+                    using (StreamReader sr = new StreamReader(url, Encoding.UTF8))
+                    {
+                        baseTxt = sr.ReadToEnd();
+                    }
+                    List<string> allTitle = new List<string>();
+                    List<int> keyNum = new List<int>();
+                    Regex reg = new Regex("第(.*?)章");
+                    MatchCollection mcs = reg.Matches(baseTxt);
+                    string[] list = reg.Split(baseTxt);
+                    List<string> newArray = list.ToList();
+                    newArray.RemoveAt(0);
+                    foreach (Match mc in mcs)
+                    {
+                        string t1 = mc.Groups[0].Value;
+                        string title = mc.Groups[1].Value;
+                        allTitle.Add(title);
+                    }
+                    for (int i = 0; i < mcs.Count; i++)
+                    {
+                        string key = (i + 1) + "-" + mcs[i].Groups[0].Value;
+                        keyTxtDic.Add(key, newArray[i * 2 + 1]);
+                        listBox1.Items.Add(key);
+                    }
+                    if (File.Exists(@"C:\Logs\TxtLogs.log"))
+                    {
+                        string ReadMark = File.ReadAllText(@"C:\Logs\TxtLogs.log");
+                        if (!string.IsNullOrEmpty(ReadMark))
+                        {
+                            textBox.Text = keyTxtDic[ReadMark];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SaveErrLog(ex.Message);
+            }
+
+        }
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                int index = listBox1.IndexFromPoint(e.X, e.Y);
+                listBox1.SelectedIndex = index;
+                if (listBox1.SelectedIndex != -1)
+                {
+                    textBox.Text = keyTxtDic[listBox1.SelectedItem.ToString()];
+                }
+            }
+            catch (Exception ex)
+            {
+
+                SaveErrLog(ex.Message);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (e.KeyChar.ToString().ToUpper() == Keys.Z.ToString())
+                {
+                    Hide();
+                }
+                else if (e.KeyChar.ToString() == "\u001b")
+                {
+                    Save();
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                SaveErrLog(ex.Message);
+            }
+        }
+
+        private void 书签ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Save();
+            }
+            catch (Exception ex)
+            {
+                SaveErrLog(ex.Message);
+            }
+        }
+        private void Save()
+        {
+            string SaveMark = listBox1.SelectedItem.ToString();
+            File.WriteAllText(@"C:\Logs\TxtLogs.log", SaveMark, Encoding.UTF8);
+        }
+        private void SaveErrLog(string log)
+        {
+            FileStream file = File.OpenWrite(@"C:\Logs\YDErrLog.log");
+            byte[] data = Encoding.Default.GetBytes(log + "\r\n");
+            file.Write(data, 0, data.Length);
+            file.Flush();
+            file.Close();
+        }
+    }
+}
